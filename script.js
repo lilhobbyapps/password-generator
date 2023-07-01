ffc014f35590a6815c973cb0792d1278c81d73e0
Lil Hobby <gregbedford93@gmail.com>
2023-07-01 16:05:29 -0400

    added comments to javascript file

--- script.js
+++ script.js
@@ -4,11 +4,24 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var genPassword;
+
+
+
+
+
+
+  genPassword = "Verga";
+  return genPassword;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+// final password
   passwordText.value = password;
 
 }
